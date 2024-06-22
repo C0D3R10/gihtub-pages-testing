667d4044cc2dc396e67c2b6f4aefb3479929167d
C0D3R10 - AJ <ammon.j.james@gmail.com>
2024-06-22 09:33:22 -0400

    Re-organizing file hiearchy

--- website_v0.1/homepage.js
+++
@@ -1,8 +0,0 @@
-let userName
-//let age = window.prompt("How old are you?");
-
-document.getElementById("b3").onclick = function(){
-    userName = document.getElementById("input1").value;
-    document.getElementById("h35").textContent = `hello ${userName}`
-}
-
